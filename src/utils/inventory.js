4adc3040892ad3c2898f9adea9da4b78a079f4bb
peacebonez <kmpariso12@gmail.com>
2020-05-28 19:20:19 -0400

    Fixed some responsiveness issues

--- src/utils/inventory.js
+++ src/utils/inventory.js
@@ -2,21 +2,21 @@ export const nukes = [
   {
     img: "https://i.imgur.com/s48I5SE.jpg",
     name: "Big Bertha",
-    description: "All around utility nuke.  Versatile and effective",
+    description: "Birth-a-destruction with this Big Bertha bomb.",
     price: 99999,
   },
   {
     img: "https://i.imgur.com/clr8EC0.jpg",
     name: "Truman's Timebomb",
     description:
-      "Be inspired by the president who was the domino that started it all!",
+      "Inspired by the presidential first domino that started it all!",
     price: 99,
   },
   {
     img: "https://i.imgur.com/QD1YJFE.jpg",
     name: "The Guardian",
     description:
-      "Sponsored by The Guardian, used to inflict damage on critics of journalism and media.",
+      "Sponsored by The Guardian to destroy critics of journalism and media.",
     price: 9999,
   },
   {
@@ -29,26 +29,27 @@ export const nukes = [
   {
     img: "https://i.imgur.com/fH4KQTZ.jpg",
     name: "Crash Dummy",
-    description: "Crash head first into the enemy",
+    description: "Crash head first into the enemy!",
     price: 650000,
   },
   {
     img: "https://i.imgur.com/DdZksPp.jpg",
     name: "Red Rocket",
     description:
-      "Penetrate your way right into the enemy's defenses with the Red Rocket.",
+      "Penetrate your way into the enemies' defenses with the Red Rocket.",
     price: 100000,
   },
   {
     img: "https://i.imgur.com/dLZFXJs.jpg",
     name: "The Elder",
-    description: "The Elder is an experienced product",
+    description: "The Elder is the premiere, experienced product.",
     price: 899999,
   },
   {
     img: "https://i.imgur.com/CVQrzMj.jpg",
     name: "Fat Man",
-    description: "The infamous Fat Man reissue. Purchase while supplies last.",
+    description:
+      "The infamous Fat Man reissue. Purchase while supplies still last.",
     price: 750000,
   },
 ];
